db3b36efbfeedeffceb5aa8dfa9a924651fb1fcd
Pedrobolfute <pepenike-101@hotmail.com>
2023-05-16 12:13:44 -0300

    constantes declarada

---
+++ assets/js/hoje.js
@@ -0,0 +1,5 @@
+const buttonTodoLeft = document.getElementsByClassName('buttonTodoLeft')[0]
+
+const buttonTodoRight = document.getElementsByClassName('buttonTodoRight')[0]
+
+const seeWeek = document.getElementsByClassName('seeWeek')[0]
