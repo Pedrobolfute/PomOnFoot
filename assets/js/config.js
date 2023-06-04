1df52c055467abe518fa2eda6117284712e60174
Pedrobolfute <pepenike-101@hotmail.com>
2023-06-04 18:01:28 -0300

    added new bell and titles

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-function changeMute() {
+function changeMusic() {
   let click = 0
   const mute = document.querySelector('.config span:nth-child(1) img')
   mute.addEventListener('click', function () {
@@ -8,7 +8,26 @@ function changeMute() {
     } else {
       click = -1
       mute.setAttribute('src', './assets/img/svg/mute.svg')
+      mute.setAttribute('title', 'Ligar Musica')
     }
   })
 }
-changeMute()
+changeMusic()
+
+
+function changeBell() {
+  let click = 0
+  const mute = document.querySelector('.config span:nth-child(5) img')
+  mute.addEventListener('click', function () {
+    click++
+    if (click === 0) {
+      mute.setAttribute('src', './assets/img/svg/bell.svg')
+    } else {
+      click = -1
+      mute.setAttribute('src', './assets/img/svg/bellMute.svg')
+      mute.setAttribute('title', 'Ligar Alarme')
+
+    }
+  })
+}
+changeBell()
