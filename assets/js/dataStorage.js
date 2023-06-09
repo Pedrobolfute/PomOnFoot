94d4c3e873aa968373bf06003717034d8ffe41b0
Pedrobolfute <pepenike-101@hotmail.com>
2023-06-09 18:05:14 -0300

    set and get localStorage added on main page

--- assets/js/dataStorage.js
+++ assets/js/dataStorage.js
@@ -1,1 +1,53 @@
-// Archive related with localStorage
+const date = new Date()
+const today = date.getDay()
+const tomorrow = today + 1
+const yesterday = today - 1
+
+function whichTodoDay() {
+  if(document.querySelector('h6')){
+  const whoScreenIs = document.querySelector('h6').textContent.toLowerCase()
+  if(whoScreenIs === 'hoje'){
+    return today
+  }else if(whoScreenIs === 'amanhã'){
+    return tomorrow
+  }else if(whoScreenIs === 'ontem'){
+    return yesterday
+  }
+}
+}
+
+function fromWeek(){
+  return week = {
+    'monday': 1,
+    'tuesday': 2,
+    'wednesday': 3,
+    'thursday': 4,
+    'friday': 5,
+    'saturday': 6,
+    'sunday': 7
+  }
+}
+
+function setTodoData(value){
+  let tasks = Array()
+  if(localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)){
+    tasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
+  }
+  tasks.push({task: value})
+  localStorage.setItem(`tasks${whichTodoDay()}`, JSON.stringify(tasks))
+}
+
+function getTodoData(){
+  let list = document.getElementsByClassName('list')[0]
+  if(localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)){
+    let myTasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
+    myTasks.forEach(task => {
+      console.log(task.task)
+      const element = `
+      <div class="formItem">
+        <input type="checkbox" class="taskList" name="${task.task}" id="${task.task}">
+        <label for="${task.task}"></label>${task.task}</div>`
+        list.innerHTML += element
+    })
+  }
+}
--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,36 +1,27 @@
-
 const insert = document.getElementsByClassName('insert')[0];
 const list = document.getElementsByClassName('list')[0];
 
+window.addEventListener('load', () => {
+getTodoData()
 insert.onsubmit = function(parameter){
-  
   parameter.preventDefault();
   const inputAdd = document.getElementsByClassName('inputAdd')[0];
-
+  
   if(inputAdd.value.trim() !== ''){
-  newElement(inputAdd.value);
-  insert.reset();
+    setTodoData(inputAdd.value)
+    newElement(inputAdd.value)
+    insert.reset();
   }
 }
 
 let newElement = function newTask(description){
-  const createDiv = document.createElement("div");
-  const createContent = document.createElement("input");
-  const createLabel = document.createElement("label");
-  const descriptionInput = document.createTextNode(description);
-
-  createContent.setAttribute("type", "checkbox");
-  createContent.setAttribute("class", "taskList");
-  createContent.setAttribute("name", description);
-  createContent.setAttribute("id", description);
-
-  createLabel.setAttribute("for", description);
-  createLabel.appendChild(descriptionInput);
-
-  createDiv.classList.add("formItem");
-  createDiv.appendChild(createContent);
-  createDiv.appendChild(createLabel);
-  createDiv.appendChild(descriptionInput);
-  
-  list.appendChild(createDiv);
+  let list = document.getElementsByClassName('list')[0]
+  const element = `
+  <div class="formItem">
+    <input type="checkbox" class="taskList" name="${description}" id="${description}">
+    <label for="${description}"></label>${description}</div>`
+  list.innerHTML += element
 }
+//console.log(whichTodoDay())
+})
+
