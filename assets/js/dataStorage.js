d0dd35c6d869667063c6d8ebbdc4140b9e7d22bb
Pedrobolfute <pepenike-101@hotmail.com>
2023-06-10 18:00:06 -0300

    checkpoint i will take off others pages

--- assets/js/dataStorage.js
+++ assets/js/dataStorage.js
@@ -4,19 +4,19 @@ const tomorrow = today + 1
 const yesterday = today - 1
 
 function whichTodoDay() {
-  if(document.querySelector('h6')){
-  const whoScreenIs = document.querySelector('h6').textContent.toLowerCase()
-  if(whoScreenIs === 'hoje'){
-    return today
-  }else if(whoScreenIs === 'amanhã'){
-    return tomorrow
-  }else if(whoScreenIs === 'ontem'){
-    return yesterday
+  if (document.querySelector('h6')) {
+    const whoScreenIs = document.querySelector('h6').textContent.toLowerCase()
+    if (whoScreenIs === 'hoje') {
+      return today
+    } else if (whoScreenIs === 'amanhã') {
+      return tomorrow
+    } else if (whoScreenIs === 'ontem') {
+      return yesterday
+    }
   }
 }
-}
 
-function fromWeek(){
+function fromWeek() {
   return week = {
     'monday': 1,
     'tuesday': 2,
@@ -28,26 +28,59 @@ function fromWeek(){
   }
 }
 
-function setTodoData(value){
+function setTodoData(value) {
   let tasks = Array()
-  if(localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)){
+  if (localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)) {
     tasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
   }
-  tasks.push({task: value})
+  tasks.push({ task: value })
   localStorage.setItem(`tasks${whichTodoDay()}`, JSON.stringify(tasks))
 }
 
-function getTodoData(){
+function loadLastTodoData(){
+    let list = document.getElementsByClassName('list')[0]
+    if (localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)) {
+      let myTasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
+      let last = myTasks[myTasks.length-1].task
+        console.log(last)
+        const element = `
+        <div class="formItem">
+          <input type="checkbox" class="taskList" name="${last}" id="${last}">
+          <label for="${last}">${last}</label></div>`
+        list.innerHTML += element
+    }
+  }
+
+
+function getTodoData() {
   let list = document.getElementsByClassName('list')[0]
-  if(localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)){
+  if (localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)) {
     let myTasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
     myTasks.forEach(task => {
-      console.log(task.task)
+      // console.log(task.task)
       const element = `
       <div class="formItem">
         <input type="checkbox" class="taskList" name="${task.task}" id="${task.task}">
-        <label for="${task.task}"></label>${task.task}</div>`
-        list.innerHTML += element
+        <label for="${task.task}">${task.task}</label></div>`
+      list.innerHTML += element
     })
   }
 }
+
+function removeTodoData(item) {
+  // debugger
+  if (item) {
+    let itemValue = item.getAttribute('name')
+    if (localStorage.hasOwnProperty(`tasks${whichTodoDay()}`)) {
+      let myTasks = JSON.parse(localStorage.getItem(`tasks${whichTodoDay()}`))
+
+      myTasks = myTasks.filter(task => {
+        return task.task !== `${itemValue}`
+      })
+      console.log(myTasks)
+
+      localStorage.setItem(`tasks${whichTodoDay()}`, JSON.stringify(myTasks))
+    }
+  }
+}
+
--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,27 +1,30 @@
 const insert = document.getElementsByClassName('insert')[0];
 const list = document.getElementsByClassName('list')[0];
+function main() {
+    getTodoData()
+    insert.onsubmit = function (parameter) {
+      parameter.preventDefault();
+      const inputAdd = document.getElementsByClassName('inputAdd')[0];
 
-window.addEventListener('load', () => {
-getTodoData()
-insert.onsubmit = function(parameter){
-  parameter.preventDefault();
-  const inputAdd = document.getElementsByClassName('inputAdd')[0];
-  
-  if(inputAdd.value.trim() !== ''){
-    setTodoData(inputAdd.value)
-    newElement(inputAdd.value)
-    insert.reset();
-  }
+      if (inputAdd.value.trim() !== '') {
+        setTodoData(inputAdd.value)
+        loadLastTodoData()
+        insert.reset();
+      }
+    }
+
 }
 
-let newElement = function newTask(description){
-  let list = document.getElementsByClassName('list')[0]
-  const element = `
-  <div class="formItem">
-    <input type="checkbox" class="taskList" name="${description}" id="${description}">
-    <label for="${description}"></label>${description}</div>`
-  list.innerHTML += element
+function load(){
+  let checkboxList = document.querySelectorAll('input[type="checkbox"]')
+  checkboxList.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      removeTodoData(checkbox)
+    })
+  })  
 }
-//console.log(whichTodoDay())
+
+window.addEventListener('DOMContentLoaded', () => {
+  main()
+  load()
 })
-
