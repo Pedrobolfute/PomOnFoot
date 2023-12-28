aba7c622326d33806dca7e94235326668cf9915a
Pedrobolfute <pepenike-101@hotmail.com>
2023-12-27 21:21:13 -0300

    minified refresh

--- src/assets/js/moveIcon.js
+++ src/assets/js/moveIcon.js
@@ -1,62 +1,31 @@
-let toy = []
-toy[0] = `
+let toy=[];toy[0]=`
 *_* <br>
 /|\\ <br>
 / \\ <br>
 ... <br>
-`
-
-toy[1] = `
+`,toy[1]=`
 *_* <br>
 /|\\ <br>
 | | <br>
 .., <br>
-`
-
-toy[2] = `
+`,toy[2]=`
 *_^ <br>
 /|\\ <br>
 / \\ <br>
 .,. <br>
-`
-toy[3] = `
+`,toy[3]=`
 *_* <br>
 /|\\ <br>
 | | <br>
 ,.. <br>
-`
-
-toy[4] = `
+`,toy[4]=`
 *_* <br>
 /|\\  <br>
 / \\ <br>
 .., <br>
-`
-
-toy[5] = `
+`,toy[5]=`
 *_* <br>
 /|\\  <br>
 | | <br>
 ,.. <br>
-`
-
-let toyPosition = 0
-function animationBoy() {
-  const icon = document.querySelector('.columnLeft h6')
-  icon.innerHTML = toy[toyPosition]
-  toyPosition += 1
-  if (toyPosition > toy.length - 1) { toyPosition = 0 }
-}
-
-
-let k = true
-function animationTimer() {
-  let time = document.querySelector('.dial-plate .time')
-  if (k) {
-    time.style.transform = 'scale(1.0)'
-    k = false
-  } else {
-    time.style.transform = 'scale(1.1)'
-    k = true
-  }
-}
+`;let toyPosition=0;function animationBoy(){let t=document.querySelector(".columnLeft h6");t.innerHTML=toy[toyPosition],(toyPosition+=1)>toy.length-1&&(toyPosition=0)}let k=!0;function animationTimer(){let t=document.querySelector(".dial-plate .time");k?(t.style.transform="scale(1.0)",k=!1):(t.style.transform="scale(1.1)",k=!0)}
